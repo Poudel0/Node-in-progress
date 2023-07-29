05c8f8a857e275cdbf7cf10aaa35d8ba75b1bc8e
Poudel0 <poudel0sandesh@gmail.com>
2023-07-29 21:42:57 +0545

    Basic POST && GET

--- NON projects/httpserver.js
+++ NON projects/httpserver.js
@@ -1,17 +1,40 @@
+// const { json } = require('express');
 const http = require('http');
 const PORT = 3000;
 const server = http.createServer();
+const jsonData = [
+  {
+    id: 101,
+    name: 'Sandman',
+  },
+  {
+    id: 102,
+    name: 'Musiman',
+  },
+  {
+    id: 103,
+    name: 'Tusiman',
+  },
+];
 server.on('request', (req, res) => {
-  if (req.url === '/json') {
+  const items = req.url.split('/'); // /json/2 => ['','json','2']
+  if (req.method == 'POST' && items[1] == 'json') {
+    req.on('data', (data) => {
+      const jsonData1 = data.toString();
+      console.log('Request:', jsonData1);
+      jsonData.push(JSON.parse(jsonData1));
+    });
+    req.pipe(res);
+  } else if (req.method == 'GET' && items[1] === 'json') {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'application/json');
-    res.end(
-      JSON.stringify({
-        id: 101,
-        name: 'Sandman',
-      })
-    );
-  } else if (req.url === '/htm') {
+    if (items.length == 3) {
+      const jsonIndex = Number(items[2]); /// Add '+' sign before string
+      res.end(JSON.stringify(jsonData[jsonIndex]));
+    } else {
+      res.end(JSON.stringify(jsonData));
+    }
+  } else if (req.method == 'GET' && req.url === '/htm') {
     res.setHeader('Content-Type', 'text/html');
     res.write('<html>');
     res.write('<body>');
