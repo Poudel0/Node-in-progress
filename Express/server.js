702112b0de5b7abe48cf86cd8ad601ba7e57b2ce
Poudel0 <poudel0sandesh@gmail.com>
2023-07-31 07:39:03 +0545

    Practiced with POSTs and Middleware

--- Express/server.js
+++ Express/server.js
@@ -6,8 +6,33 @@ const PORT = 3000;
 
 const jsonData = [
   { id: 0, name: "sandman" },
-  { id: 1, name: "musiman" },
+  { id: 1, name: "musimann" },
 ];
+
+app.use((req, res, next) => {
+  const start = Date.now();
+  console.log(`${req.method}  ${req.url}`);
+  next();
+  const delta = Date.now() - start;
+  console.log(delta);
+});
+
+app.use(express.json());
+
+app.post("/jsonData", (req, res) => {
+  if (!req.body.name) {
+    return res.status(400).json({
+      error: "Name not entered",
+    });
+  }
+  const newjsonData = {
+    name: req.body.name,
+    id: jsonData.length,
+  };
+  jsonData.push(newjsonData);
+  res.json(newjsonData);
+});
+
 app.get("/jsonData", (req, res) => {
   res.json(jsonData);
 });
